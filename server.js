6d6ce4f6baad49e9bbd718c766cfeaee37419fc1
sarooo17 <130158714+sarooo17@users.noreply.github.com>
2025-06-18 18:16:40 +0200

    fix: aggiungere opzioni di caching per le risorse statiche nel server

--- server.js
+++ server.js
@@ -205,7 +205,10 @@ async function sendThankYouEmail(userEmail, suggestion) {
 
 
 const path = require('path');
-app.use('/assets', express.static(path.join(__dirname, 'assets')));
+app.use('/assets', express.static(path.join(__dirname, 'assets'), {
+  maxAge: '1y',
+  immutable: true
+}));
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
